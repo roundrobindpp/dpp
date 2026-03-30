8d2159d39ee41901deed0d53db0b906d69fac9fe
deploy.adm <deploy.adm@gmail.com>
2026-03-30 11:48:05 +0000

    R1.5 Updating spring-boot version from 2.7.17 to actual 4.0.5, run on embedded Tomcat instead of Undertow, updating other dependencies, add role_type field for custom_roles request

--- src/main/java/com/roundrobin_assignment/dpp/Proxy.java
+++ src/main/java/com/roundrobin_assignment/dpp/Proxy.java
@@ -1,10 +1,8 @@
 package com.roundrobin_assignment.dpp;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import javax.servlet.http.HttpServlet;
-
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
